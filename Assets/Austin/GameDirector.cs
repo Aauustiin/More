9a8c1ffa253bdbe3bad60984d3320ca901c26403
Aauustiin <longaustin2001@gmail.com>
2023-02-26 17:42:18 +0000

    People are generated

---
+++ Assets/Austin/GameDirector.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    [SerializeField] private Slider globalismSlider;
+    [SerializeField] private Slider urbanismSlider;
+    [SerializeField] private Slider statismSlider;
+    [SerializeField] private Slider innovationSlider;
+    [SerializeField] private Slider marketsSlider;
+    
+    [SerializeField] private GameObject mainMenu;
+
+    [SerializeField] private ICityPlanner _cityPlanner;
+    [SerializeField] private IBuildingGenerator _buildingGenerator;
+    [SerializeField] private PeopleGenerator _peopleGenerator;
+
+    private List<GameObject> _objects;
+
+    public void Start()
+    {
+        _objects = new List<GameObject>();
+    }
+
+    public void OnDone()
+    {
+        CityStats stats = new CityStats();
+        stats.Globalism = globalismSlider.value;
+        stats.Urbanism = urbanismSlider.value;
+        stats.Statism = statismSlider.value;
+        stats.Innovation = innovationSlider.value;
+        stats.Markets = marketsSlider.value;
+
+        mainMenu.SetActive(false);
+        
+        GameObject[] people = _peopleGenerator.GeneratePeople(stats);
+        
+        //CityPlan cityPlan = _cityPlanner.GenerateCity(stats);
+        //for (int i = 0; i < cityPlan.BuildingRequirements.Length; i++)
+        //{
+        //    GameObject buildingPrefab = _buildingGenerator.GenerateBuilding(stats, cityPlan.BuildingRequirements[i].Item2);
+        //    GameObject building = Instantiate(buildingPrefab, cityPlan.BuildingRequirements[i].Item1);
+        //    building.transform.parent = transform;
+        //    _objects.Add(building);
+        //}
+        //GameObject road = Instantiate(cityPlan.Road, transform);
+        //_objects.Add(road);
+        
+        //_objects.Add(people);
+    }
+}
---
+++ Assets/Austin/PeopleGenerator.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleGenerator : MonoBehaviour, IPeopleGenerator
+{
+    private int _population = 10;
+    [SerializeField] private GameObject personPrefab;
+    [SerializeField] private Shader defaultShader;
+    
+    public GameObject[] GeneratePeople(CityStats stats)
+    {
+        GameObject[] people = new GameObject[100];
+        for (int i = 0; i < _population; i++)
+        {
+            people[i] = Instantiate(personPrefab, transform);
+        }
+
+        GenerateCharacterColours(stats, people);
+        return people;
+    }
+
+    public void GenerateCharacterColours(CityStats stats, GameObject[] personObjects)
+    {
+        List<Color> characterColours = new List<Color> {};
+        Color nativeColour = Random.ColorHSV();
+        for (int i = 0; i < _population; i++)
+        {
+            if (stats.Globalism < Random.value)
+            {
+                MeshRenderer[] meshes = personObjects[i].GetComponentsInChildren<MeshRenderer>();
+                Color color = Random.ColorHSV();
+                Material mat = new Material(defaultShader);
+                mat.SetColor("_Color", color);
+                for (int j = 0; j < meshes.Length; j++)
+                {
+                    meshes[j].material = mat;
+                }
+            }
+            else
+            {
+                MeshRenderer[] meshes = personObjects[i].GetComponentsInChildren<MeshRenderer>();
+                Color color = Random.ColorHSV();
+                Material mat = new Material(defaultShader);
+                mat.SetColor("_Color", nativeColour);
+                for (int j = 0; j < meshes.Length; j++)
+                {
+                    meshes[j].material = mat;
+                }
+            }
+        }
+    }
+}
