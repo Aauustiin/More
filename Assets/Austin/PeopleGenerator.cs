4310a00e6be0fbd2a84193ec9fd42eb0935f67fe
Aauustiin <longaustin2001@gmail.com>
2023-02-26 17:58:21 +0000

    Intermediary commit

--- Assets/Austin/PeopleGenerator.cs
+++ Assets/Austin/PeopleGenerator.cs
@@ -6,17 +6,20 @@ public class PeopleGenerator : MonoBehaviour, IPeopleGenerator
     private int _population = 10;
     [SerializeField] private GameObject personPrefab;
     [SerializeField] private Shader defaultShader;
-    
+
     public GameObject[] GeneratePeople(CityStats stats)
     {
-        GameObject[] people = new GameObject[100];
-        for (int i = 0; i < _population; i++)
+        GameObject[] people = new GameObject[_population];
+        if (stats.Urbanism > 0.5)
         {
-            people[i] = Instantiate(personPrefab, transform);
+            for (int i = 0; i < (_population * stats.Urbanism); i++)
+            {
+                people[i] = Instantiate(personPrefab, transform);
+            }
         }
-
         GenerateCharacterColours(stats, people);
         return people;
+        
     }
 
     public void GenerateCharacterColours(CityStats stats, GameObject[] personObjects)
