b9d1079766a009ef24d2f906798c148dd67e304b
Aauustiin <longaustin2001@gmail.com>
2023-02-27 08:54:06 +0000

    Made thoughts appear

--- Assets/Austin/GameDirector.cs
+++ Assets/Austin/GameDirector.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.Numerics;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using Random = System.Random;
 using Vector2 = UnityEngine.Vector2;
-using Vector3 = System.Numerics.Vector3;
 
 public class GameDirector : MonoBehaviour
 {
@@ -17,13 +15,17 @@ public class GameDirector : MonoBehaviour
     
     [SerializeField] private GameObject mainMenu;
 
-    [SerializeField] private MockCityPlanner _cityPlanner;
-    [SerializeField] private BuildingGenerator _buildingGenerator;
-    [SerializeField] private PeopleGenerator _peopleGenerator;
+    [SerializeField] private MockCityPlanner cityPlanner;
+    [SerializeField] private BuildingGenerator buildingGenerator;
+    [SerializeField] private PeopleGenerator peopleGenerator;
+    [SerializeField] private GenerateSpeech speechGenerator;
 
     [SerializeField] private Vector2 minBound;
     [SerializeField] private Vector2 maxBound;
 
+    [SerializeField] private GameObject thoughtBubble;
+    [SerializeField] private Camera mainCamera;
+    
     private List<GameObject> _objects;
 
     public void Start()
@@ -42,17 +44,21 @@ public class GameDirector : MonoBehaviour
 
         mainMenu.SetActive(false);
         
-        GameObject[] people = _peopleGenerator.GeneratePeople(stats);
+        GameObject[] people = peopleGenerator.GeneratePeople(stats);
+        string[] thoughts = speechGenerator.GenerateThoughts(stats, people.Length);
         for (int i = 0; i < people.Length; i++)
         {
             people[i].transform.position = new UnityEngine.Vector3(UnityEngine.Random.Range(minBound.x, maxBound.x), 0.5f,UnityEngine.Random.Range(minBound.y, maxBound.y));
             people[i].transform.rotation = UnityEngine.Quaternion.Euler(0f, UnityEngine.Random.Range(0f, 360f), 0f);
+            GameObject thought = Instantiate(thoughtBubble, people[i].transform);
+            thought.transform.GetChild(0).GetChild(0).position = mainCamera.WorldToScreenPoint(people[i].transform.position + new UnityEngine.Vector3(0, 0f, 1));
+            thought.GetComponentInChildren<TextMeshProUGUI>().text = thoughts[i];
         }
 
-        CityPlan cityPlan = _cityPlanner.GenerateCity(stats);
+        CityPlan cityPlan = cityPlanner.GenerateCity(stats);
         for (int i = 0; i < cityPlan.BuildingRequirements.Length; i++)
         {
-            GameObject buildingPrefab = _buildingGenerator.GenerateBuilding(stats, cityPlan.BuildingRequirements[i].Item2);
+            GameObject buildingPrefab = buildingGenerator.GenerateBuilding(stats, cityPlan.BuildingRequirements[i].Item2);
             GameObject building = Instantiate(buildingPrefab, cityPlan.BuildingRequirements[i].Item1);
             building.transform.localScale = new UnityEngine.Vector3(5f, 5f, 5f);
             building.transform.position = new UnityEngine.Vector3(UnityEngine.Random.Range(minBound.x, maxBound.x), 0.5f,UnityEngine.Random.Range(minBound.y, maxBound.y));
@@ -62,5 +68,16 @@ public class GameDirector : MonoBehaviour
         }
         //GameObject road = Instantiate(cityPlan.Road, transform);
         //_objects.Add(road)
+    }
+
+    private void Update()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out RaycastHit hit)) {
+            Transform objectHit = hit.transform;
+            
+            // Do something with the object that was hit by the raycast.
+        }
     }
 }
